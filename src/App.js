0708251add8b031896c602d8602eafe8dc5a4fa4
Sanjay Saravanan <ssaravanan6384@altimetrik.com>
2021-09-28 12:27:56 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +16,51 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
+        <FunctionComponentExample />
       </header>
     </div>
   );
 }
+
+const FunctionComponentExample = () => {
+  const [count, setCount] = useState(0);
+  const [isLiked, setLiked] = useState(false);
+  const [color, setColor] = useState("#fff");
+
+  // Mounting Phase
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    // Unmount
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  // Updating Phase
+  useEffect(() => {
+    alert(`Like is ${isLiked}`);
+    if (isLiked) setColor("blue");
+    else setColor("#fff");
+  }, [isLiked]);
+
+  return (
+    <>
+      <button
+        style={{ height: "30px", width: "30px", backgroundColor: "grey" }}
+        onClick={() => setCount(count + 1)}
+      >
+        {count}
+      </button>
+      <button style={{ color: color }} onClick={() => setLiked(!isLiked)}>
+        Like
+      </button>
+    </>
+  );
+};
 
 export default App;
